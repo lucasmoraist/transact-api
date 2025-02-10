75e3ea56579567821db2241880402f19402e7d49
lucasmoraist <luksmnt1101@gmail.com>
2025-02-10 11:33:36 -0300

    Add initial gateway configuration and setup files

---
+++ gateway/src/main/java/com/challenge/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.challenge.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RestClient.Builder restClient() {
+		return RestClient.builder();
+	}
+}
