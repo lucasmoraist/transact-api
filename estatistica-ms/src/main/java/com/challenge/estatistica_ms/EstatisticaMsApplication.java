1702b5b3a9013ec51904322cda593befff952d74
lucasmoraist <luksmnt1101@gmail.com>
2025-02-10 13:23:05 -0300

    Add OpenAPI documentation support for Estatistica and Transacao services

--- estatistica-ms/src/main/java/com/challenge/estatistica_ms/EstatisticaMsApplication.java
+++ estatistica-ms/src/main/java/com/challenge/estatistica_ms/EstatisticaMsApplication.java
@@ -1,9 +1,16 @@
 package com.challenge.estatistica_ms;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@OpenAPIDefinition(info = @Info(
+		title = "Estatistica API",
+		version = "1.0",
+		description = "API para estatísticas"
+))
 @SpringBootApplication
 @EnableFeignClients
 public class EstatisticaMsApplication {
