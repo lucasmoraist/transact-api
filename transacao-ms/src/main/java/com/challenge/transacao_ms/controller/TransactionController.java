240734ff860eec79af6dd26e92d0ca2493fa1b42
lucasmoraist <luksmnt1101@gmail.com>
2025-02-10 13:23:40 -0300

    Remove unused import statement from TransactionController

--- transacao-ms/src/main/java/com/challenge/transacao_ms/controller/TransactionController.java
+++ transacao-ms/src/main/java/com/challenge/transacao_ms/controller/TransactionController.java
@@ -7,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
